fa9d08cfcbc8601a38731a682a94a5735c53e811
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-12-19 04:28:46 +0100

    Initial Commit

---
+++ Swifty Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifty Weather
+//
+//  Created by Victor Loveday on 19/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swifty Weather/Swifty_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_WeatherApp.swift
+//  Swifty Weather
+//
+//  Created by Victor Loveday on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
