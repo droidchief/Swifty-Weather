6a476e2ce18ad9dd9f47e214e803f000456c28d9
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-12-19 05:15:47 +0100

    Feat: gradient background

--- Swifty Weather/ContentView.swift
+++ Swifty Weather/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white, .white]), startPoint: .topTrailing, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
